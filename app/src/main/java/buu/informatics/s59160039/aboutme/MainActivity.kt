b16d5e86c4274fb852e9a99428a9bcc1354bb586
wittayakaew <59160039@go.buu.ac.th>
2019-08-18 15:28:50 +0700

    add actions

--- app/src/main/java/buu/informatics/s59160039/aboutme/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160039/aboutme/MainActivity.kt
@@ -8,6 +8,7 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +17,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         findViewById<Button>(R.id.done_button).setOnClickListener {
             addNickname(it)
+        }
+        findViewById<TextView>(R.id.nickname_text).setOnClickListener(){
+            updateNickname(it)
         }
     }
 
@@ -33,5 +37,20 @@ class MainActivity : AppCompatActivity() {
     //hide the keyboard
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(view.windowToken,0)
+    }
+
+    private fun updateNickname(view: View){
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        editText.visibility = View.VISIBLE
+        nicknameTextView.visibility = View.GONE
+
+        val doneBuuton = findViewById<Button>(R.id.done_button)
+        doneBuuton.visibility = View.VISIBLE
+
+        editText.requestFocus()
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.showSoftInput(editText,0)
     }
 }
